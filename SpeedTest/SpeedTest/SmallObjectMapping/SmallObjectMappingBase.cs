42b8d09ac8cd444e7651b59f5579828973bfb269
Sobieck00 <thomas@sobieck.com>
2016-10-02 11:04:28 -0500

    New results. Done for the day.

--- SpeedTest/SpeedTest/SmallObjectMapping/SmallObjectMappingBase.cs
+++ SpeedTest/SpeedTest/SmallObjectMapping/SmallObjectMappingBase.cs
@@ -4,6 +4,6 @@ namespace SpeedTest.SmallObjectMapping
 {
     public abstract class SmallObjectMappingBase : SmallObjectBase
     {
-        public override string TypeOfTest { get { return TypesOfTest.SmallObjectMapping; } }
+        public override string TypeOfTest { get { return "Small Object Mapping"; } }
     }
 }
--- SpeedTest/SpeedTest/TypesOfTest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SpeedTest
-{
-    public struct TypesOfTest
-    {
-        public static string SmallObjectSerialization { get { return "Small Object Serialization"; } }
-
-        public static string SmallObjectMapping { get { return "Small Object Mapping"; } }
-    }
-}
