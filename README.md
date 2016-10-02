62898784910983b2d044ccf9ad03ede09ddc014d
Sobieck00 <thomas@sobieck.com>
2016-10-02 09:42:11 -0500

    Ran in release mode. Done optimizing.

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ A Project for easily testing the speed of various libraries.
 |Saturday, October 1, 2016|10:29:29 PM|(0) SmallNewtonSoftSerializer|19,181,952|0.01564|
 |Sunday, October 2, 2016|2:15:37 PM|(2) SmallObjectStaticMapper|1,352,630,731|0.00022|
 |Sunday, October 2, 2016|2:25:15 PM|(2) SmallObjectStaticMapper|1,456,181,861|0.00021|
+|Sunday, October 2, 2016|2:37:42 PM|(2) SmallObjectStaticMapper|3,170,566,489|0.00009|
--- SpeedTest/SpeedTest/Program.cs
+++ SpeedTest/SpeedTest/Program.cs
@@ -52,26 +52,5 @@ namespace SpeedTest
                 file.WriteLine(lineToWrite);
             }
         }
-
-        public static class NonBlockingConsole
-        {
-            public static ulong Iterations { private get; set; }
-
-            public static int TimeInMillisecondsToRun { private get; set; }
-
-            public static double MillisecondsElapsed { private get; set; }
-
-            static NonBlockingConsole()
-            {
-                Task.Run(() =>
-                {
-                    while (true)
-                    {
-                        var output = string.Format("\rRun: {0:N0} times | MS Left {1:N0}", Iterations, TimeInMillisecondsToRun - MillisecondsElapsed);
-                        Console.Out.WriteAsync(output);
-                    }
-                });
-            }
-        }
     }
 }
