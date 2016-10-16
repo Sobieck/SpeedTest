e9fc3b4243da91e4448f4e3263df621d07e9df29
Sobieck00 <thomas@sobieck.com>
2016-10-16 07:40:16 -0500

    Starting to build out the serializer tests.

---
+++ SpeedTest/SpeedTest/SmallNewtonSoftWrapper/SmallNewtonSoftWrapperSerializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpeedTest.SmallNewtonSoftWrapper
+{
+    public class SmallNewtonSoftWrapperSerializer : SmallObjectSerializerBase
+    {
+        public override string TestableAct()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SpeedTest/SpeedTest/SmallObjectSerializerBase.cs
@@ -0,0 +1,12 @@
+﻿using SnippetSpeed;
+using SpeedTest.TestObjects;
+
+namespace SpeedTest
+{
+    public abstract class SmallObjectSerializerBase : TestableSnippetSpeedBase<string>
+    {
+        public override string TypeOfTest => "Small Object Serializer";
+
+        public SmallTestObject smallObject => UniformTestObjects.SmallObject;
+    }
+}
